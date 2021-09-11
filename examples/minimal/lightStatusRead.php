e056f4d3139405e4a9f23c8bd97afed1599fb98c
martijnkunstman <martijn@kunstman.nl>
2021-09-11 22:24:23 +0200

    example minimal php files

--- examples/minimal/lightStatusRead.php
+++ examples/minimal/lightStatusRead.php
@@ -1,4 +1,4 @@
 <?php
-    //this file reads the value of the file lightStatus.txt and returns it as a valid JSON using an echo
+    //this script reads the value of the file lightStatus.txt and returns that value as a valid JSON using an echo
     echo '{"lightStatus":'.file_get_contents("lightStatus.txt")."}";
 ?>
---
+++ examples/minimal/lightStatusUpdate.php
@@ -0,0 +1,8 @@
+<?php
+    //this script writes the value of the GET variable lightStatus to the file lightStatus.txt
+    if (isset($_GET['lightStatus'])) {
+        $file = fopen("lightStatus.txt", "w");
+        fwrite($file, $_GET['lightStatus']);
+        fclose($file);
+    }
+?>
